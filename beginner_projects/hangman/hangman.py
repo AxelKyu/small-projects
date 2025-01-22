79486002dd2abfae4d158bff8073bf10e601ba6c
Rijan Bhattarai <rijanbhattarailol@gmail.com>
2025-01-22 22:48:08 +0545

    Word Length Display

--- beginner_projects/hangman/hangman.py
+++ beginner_projects/hangman/hangman.py
@@ -1,3 +1,12 @@
 import random
-from beginner_projects.hangman.word_list import words
+from word_list import words
+
+selected_word = random.choice(words)
+word_length = len(selected_word)
+i = 0
+
+while (i != word_length):
+    print ("_", end=" ")
+    i += 1
+
 
