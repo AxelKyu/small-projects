f26d66ffc1f0751f8dfc3d74509ff651caaa7b9e
Rijan Bhattarai <rijanbhattarailol@gmail.com>
2025-01-25 15:19:15 +0545

    Password Generator Completed

--- beginner_projects/pwd/pwd_mngr.py
+++ beginner_projects/pwd/pwd_mngr.py
@@ -1,13 +1,12 @@
-import random 
-import string 
+import random
+import string
 
-pwd_length = int(input("Length of Password: "))
-pwd_no = int(input("How many Passwords do you need?: "))
+pwd_length = int(input("Password Length: "))
+pwd_no = int(input("Number of Passwords: "))
 
 def pwd_generator(pwd_length, pwd_no):
-    # initialization of the stuff to make the password generator
-    characters = string.ascii_letters + string.digits + string.punctuation
     generated_passwords = []
+    characters = string.ascii_letters + string.digits + string.punctuation
 
     for i in range(pwd_no):
         password = ''.join(random.choice(characters) for i in range(pwd_length))
@@ -17,6 +16,6 @@ def pwd_generator(pwd_length, pwd_no):
 
 passwords = pwd_generator(pwd_length, pwd_no)
 
-print ("\nGenerated Passwords:")
+print("Generated Passwords:\n")
 for idx, pwd in enumerate(passwords, 1):
     print(f"{idx}: {pwd}")
